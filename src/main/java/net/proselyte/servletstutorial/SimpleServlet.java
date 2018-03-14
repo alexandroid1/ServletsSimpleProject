c35724f338f0d23bcfb60aa0ddf5f1860e0a4c92
alexandroid1 <alex123456>
2018-03-14 13:26:16 +0200

    Initial commit

---
+++ src/main/java/net/proselyte/servletstutorial/SimpleServlet.java
@@ -0,0 +1,31 @@
+package net.proselyte.servletstutorial;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class SimpleServlet extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        message = "This is simple servlet message";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter messageWriter = response.getWriter();
+        messageWriter.println("<h1>" + message + "<h1>");
+    }
+
+    public void destroy() {
+
+    }
+
+}
